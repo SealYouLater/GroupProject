b03eaa9bec83f532f81b8ad3a213a1e97fc3f284
ahsanhaleem <ahsanhaleem@cmail.carleton.ca>
2017-08-15 20:58:26 -0400

    add

--- DiscardHighPoints.java
+++ DiscardHighPoints.java
@@ -9,14 +9,14 @@ public class DiscardHighPoints extends Player {
    * @return a high point card to change suits if one exists, or the higheset
    * point card that can be played, or null if neither exist
    */
-  public Card getHighPointCard(Card lastDiscard) {
+  public Card getHighPointCard(DiscardPile pile, Card lastDiscard) {
     Card highPointCard = null;
     
     //loop over the player's hand
     for(Card thisCard : hand) {
       //LOGIC: is nextCard valid? need to check if suit matches, or rank matches
       //if not valid, continue the loop. remember 8 is always valid
-      if((thisCard.getSuit()).equals(lastDiscard.getSuit()) || (thisCard.getRank() == (lastDiscard.getRank())) || (thisCard.getRank() == 8)){
+      if(isValid(pile,lastDiscard)){
         //the card is valid to be played
         
         //if suit is diff and it is a high point card, it qualifies as a highPointSuitChange card
@@ -25,7 +25,11 @@ public class DiscardHighPoints extends Player {
         if(!thisCard.getSuit().equals(lastDiscard.getSuit()) && (thisCard.getPointValue() >= 10)) {
           return thisCard;
         }
-        // check if it has a higher point value than current highPointCard
+        //set first valid card to highPointCard to start
+		if(highPointCard == null) {
+			highPointCard = thisCard;
+		}
+		// check if it has a higher point value than current highPointCard
         if(thisCard.getPointValue() > highPointCard.getPointValue()){
           highPointCard = thisCard;
         }
@@ -38,23 +42,36 @@ public class DiscardHighPoints extends Player {
   
   public boolean play(DiscardPile discardPile, Stack<Card> drawPile, ArrayList<Player> players) {
     //look for high point cards
-    Card toPlay = getHighPointCard(discardPile.top());
+    Card toPlay = getHighPointCard(discardPile,discardPile.top());
     
     if(toPlay != null) {
       //place card in discard pile and remove from the player's hand
       //if the player's hand is empty, we win! return true
       discardPile.add(toPlay);
       hand.remove(toPlay);
-      // if the hand is empty he won!
+      // if the hand is empty we won!
       if(hand.isEmpty()){
         return true;
       }
     }
     else {
-		return false;
-      //take cards from the draw pile until valid (but could keep drawing) or drawPile empty
-      //if pile empty, skip turn (return false)
-      //if this card is playable, play it right away
+		//take cards from the draw pile until valid (but could keep drawing) or drawPile empty
+        //if pile empty, skip turn (return false)
+        //if this card is playable, play it right away
+		while(true){
+			if(drawPile.empty()){
+				break;
+			}
+			else {
+				toPlay = drawPile.pop();
+				hand.add(toPlay);
+				if(isValid(discardPile, toPlay)) {
+					hand.remove(toPlay);
+					discardPile.add(toPlay);
+				}
+			}
+		}
+
     }
     
     //if we get here we must have at least one card in our hand
