35f716905430d52d297a984cd8f814682b7ef62b
ahsanhaleem <ahsanhaleem@cmail.carleton.ca>
2017-08-15 21:51:36 -0400

    add

---
+++ MemoryPlayer.java
@@ -0,0 +1,8 @@
+public class MemoryPlayer extends Player {
+  
+  
+  public boolean play(DiscardPile discardPile, Stack<Card> drawPile, ArrayList<Player> players) {
+	//deep copy of cards
+	Card myCard(discardPile.peek());
+	Card.ArrayList[];
+  }
